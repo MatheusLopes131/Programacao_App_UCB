307adba65f241cdc8970ac6d6acc10d9582280ef
Matheus_131 <matlopes05@gmail.com>
2026-09-25 19:41:15 -0300

    correçao de nome de imagem

--- App 2 GamePlay/src/screens/AppointmentDetails.js
+++ App 2 GamePlay/src/screens/AppointmentDetails.js
@@ -24,7 +24,7 @@ const players = [
     id: '2', 
     name: 'Rodrigo Gonçalves', 
     role: 'Convidado', 
-    avatar: require('../../assets/Jogador_2.png') // Atenção ao J maiúsculo
+    avatar: require('../../assets/jogador_2.png') 
   },
   { 
     id: '3', 
